5c8aea4bcee8644dde6dea1dafab05aa534eefdd
Ethan Violette <eviolette@gmail.com>
2017-08-09 12:43:39 -0500

    changed db link

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
-    database: 'mongodb://ejv:ejv2017@ds149511.mlab.com:49511/countsapp',
+    //database: 'mongodb://ejv:ejv2017@ds149511.mlab.com:49511/countsapp',
+    database: 'mongodb://server.canete-medina.com',
     secret: 'yo secret'
 };
